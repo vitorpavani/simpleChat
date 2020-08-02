200ca2133f5e84f083531f6aa707a6fcf15735fa
Vitor Pavan <vipavani@hotmail.com>
2020-08-02 19:36:52 +0200

    peer server

--- public/script.js
+++ public/script.js
@@ -1,3 +1,11 @@
 const socket = io("/");
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
 
 socket.emit("join-room", ROOM_ID, 10);
+
+socket.on("user-connect", (userId) => {
+  console.log(userId);
+});
