09ef9d3c8fb9687966f8bbd3f0bc99b8b18c6ff2
Vitor Pavan <vipavani@hotmail.com>
2020-08-02 17:00:13 +0200

    module dotenv and port config

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var app = require("express")();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
+require("dotenv").config();
+
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -12,6 +14,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(process.env.port || 3000, () => {
-  console.log("listening on *:3000");
+http.listen(process.env.port, () => {
+  console.log(`listening on port: ${process.env.port}`);
 });
