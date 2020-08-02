277571e183e07d6abe076a6d4bbb17b8f0a78d72
Vitor Pavan <vipavani@hotmail.com>
2020-08-02 18:37:47 +0200

    scripts and room config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const http = require("http").createServer(app);
+const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const { v4: uuidV4 } = require("uuid");
 
@@ -11,12 +11,18 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
- // res.sendFile(__dirname + "/index.html");
+  // res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
 });
 
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("join-room", (roomId, userId) => {
+    console.log( roomId, userId);
+    // socket.on("chat message", (msg) => {
+    //   io.emit("chat message", msg);
   });
 });
 
