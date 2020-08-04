789389f98b9423a0dfcb4c143f24350ba922a612
Vitor Pavan <vipavani@hotmail.com>
2020-08-04 10:33:42 +0200

    video chat function

--- index.js
+++ index.js
@@ -9,19 +9,26 @@ const { v4: uuidV4 } = require("uuid");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.get("/message", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
-  // res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connect", userId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
     // socket.on("chat message", (msg) => {
     //   io.emit("chat message", msg);
   });
